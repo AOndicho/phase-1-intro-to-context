ca454fa87b43f6078f8676b5c467e0f753b25947
Ondicho <albert.ondicho@moringaschool.com>
2022-06-27 00:03:09 +0300

    commit tree 658fbaf080f7223a1327ae0e2c7c26a436537625

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+function createEmployeeRecord(array) {
+  const employeeObject = {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+  return employeeObject;
+}
+
+function createEmployeeRecords(arrays) {
+  const employeeObjectsArray = [];
+  for (let array of arrays) {
+    const employee = createEmployeeRecord(array);
+    employeeObjectsArray.push(employee);
+  }
+  return employeeObjectsArray;
+}
+
+function createTimeInEvent(employee, date) {
+  const timeInEventObject = {
+    type: "TimeIn",
+    hour: date,
+    date: date,
+  };
+  const employeeObject = createEmployeeRecord(employee);
+  employeeObject["timeInEvents"].push(timeInEventObject);
+
+  return employeeObject;
+}
+
+function createTimeOutEvent() {}
+
+function hoursWorkedOnDate() {}
+
+function wagesEarnedOnDate() {}
+
+function allWagesFor() {}
+
+function calculatePayroll() {}
